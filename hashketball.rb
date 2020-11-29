f319e19ce24aaa4313e3d1bea3fd136604f3637a
Ryan Choe <ryan.choe@outlook.com>
2020-11-29 22:22:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,9 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"]
-      :players =>[]
+      :players =>[
+        {}
+        ]
       
     }
   }
