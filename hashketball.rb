2a2a07c52ebde6507c25f7640498ec35d7628ee0
Ryan Choe <ryan.choe@outlook.com>
2020-11-29 22:29:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,8 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"]
       :players =>[
-        {:players_name => "Alan Anderson", :number => , :shoe =>},
-        {}
+        {:players_name => "Alan Anderson",:number => ,:shoe => ,:points => ,:rebounds => ,:assists => ,:steals => ,:blocks => , :slam_dunks => },
+        {:players_name => "",:number => ,:shoe => ,:points => ,:rebounds => ,:assists => ,:steals => ,:blocks => , :slam_dunks => },
         ]
       
     },
