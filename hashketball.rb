9845262358dac844cc841b997b6c6cafce18a82d
Ryan Choe <ryan.choe@outlook.com>
2020-11-29 22:33:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,11 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"]
       :players =>[
-        {:players_name => "Alan Anderson",:number => ,:shoe => ,:points => ,:rebounds => ,:assists => ,:steals => ,:blocks => , :slam_dunks => },
-        {:players_name => "",:number => ,:shoe => ,:points => ,:rebounds => ,:assists => ,:steals => ,:blocks => , :slam_dunks => },
+        {:players_name => "Alan Anderson",:number => 0,:shoe => 16 ,:points => 22,:rebounds => 12,:assists => 12,:steals => 3 ,:blocks => 1, :slam_dunks => 1},
+        {:players_name => "Reggie Evans",:number => 30,:shoe => 14,:points => 12,:rebounds => 12,:assists => 12,:steals => 12,:blocks => 12, :slam_dunks => 7},
+        {:players_name => "Brook Lopez",:number => 11,:shoe => 17,:points => 17,:rebounds => 19,:assists => 10,:steals => 3,:blocks => 1, :slam_dunks => 15},
+        {:players_name => "Mason Plumlee",:number => 1,:shoe => 19,:points => 26,:rebounds => 11,:assists => 6,:steals => 3,:blocks => 8, :slam_dunks => 5},
+        {:players_name => "",:number => ,:shoe => ,:points => ,:rebounds => ,:assists => ,:steals => ,:blocks => , :slam_dunks => }
         ]
       
     },
